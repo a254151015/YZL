a6adc73cab5b392a320b62508a485436c9017c63
a254151015 <254151015@qq.com>
2016-10-13 14:24:20 +0800

    框架修改

--- YZL.BaseController/BaseAdminController/BaseAdminController.cs
+++ YZL.BaseController/BaseAdminController/BaseAdminController.cs
@@ -8,5 +8,9 @@ namespace YZL.BaseController.BaseAdminController
 {
     public class BaseAdminController : BaseController
     {
+        public static string CurrentLoginUser { get {
+
+            return "";
+        } }
     }
 }
--- YZL/Areas/Admin/Controllers/HomeController.cs
+++ YZL/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YZL.BaseController.BaseAdminController;
 
 namespace YZL.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseAdminController//Controller
     {
         //
         // GET: /Admin/Home/
         public ActionResult Index()
         {
+            var userName = CurrentLoginUser;
             return View();
         }
 
